83e2509b4dbc6e1b12f94346772551c5bc459f83
Thanh Nga <thanhngabkav01@gmail.com>
2017-07-12 00:12:53 +0700

    Change Project Struct, Add WebApi Connection Class, Add implement service class (Uncompleted)

---
+++ ATP_IuhCoder/WebAPP/src/main/Java/Service/ICoderRatingLoader.java
@@ -0,0 +1,17 @@
+package Service;
+
+import Models.CoderRating;
+
+import java.util.List;
+
+/**
+ * Created by lldtien on 7/10/2017.
+ */
+public interface ICoderRatingLoader {
+    /**
+     * Get top coder in range order by Rating
+     * @param range
+     * @return
+     */
+    List<CoderRating> loadTopCoder(int range);
+}
---
+++ ATP_IuhCoder/WebAPP/src/main/Java/Service/IProblemRatingLoader.java
@@ -0,0 +1,17 @@
+package Service;
+
+import Models.ProblemRating;
+
+import java.util.List;
+
+/**
+ * Created by lldtien on 7/10/2017.
+ */
+public interface IProblemRatingLoader {
+    /**
+     * Get top problem in range, order by problem's rating
+     * @param range
+     * @return
+     */
+    List<ProblemRating> loadTopNewestProblem(int range);
+}
